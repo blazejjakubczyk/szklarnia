5cd8d5563eb7e3874fee62dbbb695e71f5d90a62
Blazej <jakubczyk.blazej@gmail.com>
2023-11-19 15:03:27 +0100

    teraz tylko backend zrobić

--- src/main/java/com/example/szklarnia/FertilizerManagement.java
+++ src/main/java/com/example/szklarnia/FertilizerManagement.java
@@ -1,4 +1,40 @@
 package com.example.szklarnia;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class FertilizerManagement {
+
+    private Parent fxmlLoader;
+    private Stage stage;
+    private Scene scene;
+    @FXML
+    protected void cancel(ActionEvent event) throws IOException {
+
+        fxmlLoader = FXMLLoader.load(getClass().getResource("hello-view.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(fxmlLoader);
+        stage.setTitle("Main");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    protected void save(ActionEvent event) throws IOException {
+
+        fxmlLoader = FXMLLoader.load(getClass().getResource("hello-view.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(fxmlLoader);
+        stage.setTitle("Main");
+        stage.setScene(scene);
+        stage.show();
+        /*Tu cos do zapisu danych*/
+    }
 }
